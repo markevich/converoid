858f452dd9a46d41f81ae279e3b513c25e586c45
markevich <slava.markevich@gmail.com>
2017-05-10 23:33:51 +0300

    initial

---
+++ Assets/Scripts/Hex.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hex {
+	//Q - column
+	public int Q;
+	//R - row
+	public int R;
+	public int S;
+
+	public int radius = 1;
+	public Hex(int column, int row){
+		this.Q = column;
+		this.R = row;
+		this.S = -( column + row );
+	}
+
+	public Vector3 Position(){
+		var x = HexHorizontalSpacing() * (this.Q + this.R/2f);
+		var y = HexVerticalSpacing() * this.R;
+
+		return new Vector3(x, y ,0);
+	}
+
+	public float HexWidth(){
+		return Mathf.Sqrt(3)/2f * HexHeight();
+	}
+	public int HexHeight(){
+		return radius * 2;
+	}
+	public float HexVerticalSpacing(){
+		return HexHeight() * 0.75f;
+	}
+
+	public float HexHorizontalSpacing(){
+		return HexWidth();
+	}
+}
---
+++ Assets/Scripts/HexWorld.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexWorld : MonoBehaviour {
+
+	public GameObject HexPrefab;
+	void Start () {
+		int map_radius = 10;
+
+		for (int q = -map_radius; q <= map_radius; q++) {
+			int r1 = Mathf.Max(-map_radius, -q - map_radius);
+			int r2 = Mathf.Min(map_radius, -q + map_radius);
+			for (int r = r1; r <= r2; r++) {
+				var hexn = new Hex(q, r);
+				Instantiate(HexPrefab, hexn.Position(), Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
