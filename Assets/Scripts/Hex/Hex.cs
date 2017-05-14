ef16c85a1715674c1efbce60b3a7394fad2dbf8c
markevich <slava.markevich@gmail.com>
2017-05-14 22:51:38 +0300

    Add hex mouse hover

--- Assets/Scripts/Hex/Hex.cs
+++ Assets/Scripts/Hex/Hex.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 namespace Hex{
 	public class Hex : MonoBehaviour {
 		// Use this for initialization
-		public HexPosition HexPosition;
+		public HexCoord HexCoord;
+		public bool Selected, previouslySelected = false;
 		void Start () {
 			
 		}
 		
 		// Update is called once per frame
 		void Update () {
-			
+			if(Selected){
+				GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,.3f);
+			}else if(previouslySelected){
+				GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,1f);
+			}
+			previouslySelected = Selected;
+			Selected = false;
 		}
 
 		void OnDrawGizmos()
 		{
-			this.transform.GetComponentInChildren<TextMesh>().text = HexPosition.ToString();
+			this.transform.GetComponentInChildren<TextMesh>().text = HexCoord.ToString();
 		}
 	}
 }
--- Assets/Scripts/Hex/HexWorld.cs
+++ Assets/Scripts/Hex/HexWorld.cs
@@ -10,24 +10,37 @@ namespace Hex{
 		public int MapRadius = 10;
 		void Start () {
 			World = new Dictionary<string, GameObject>();
+			
+			StartCoroutine(GenerateWorld());
+		}
+		
+		IEnumerator GenerateWorld(){
 			for (int q = -MapRadius; q <= MapRadius; q++) {
 				int r1 = Mathf.Max(-MapRadius, -q - MapRadius);
 				int r2 = Mathf.Min(MapRadius, -q + MapRadius);
 				for (int r = r1; r <= r2; r++) {
-					var hexPosition = new HexPosition(q, r);
+					var hexCoord = new HexCoord(q, r);
 
-					var instance = (GameObject)Instantiate(HexPrefab, hexPosition.Position(), Quaternion.identity, this.transform);
-					instance.GetComponent<Hex>().HexPosition = hexPosition;
+					var instance = (GameObject)Instantiate(HexPrefab, hexCoord.Position(), Quaternion.identity, this.transform);
+					instance.GetComponent<Hex>().HexCoord = hexCoord;
 
-					World.Add(hexPosition.ToString(), instance);
+					World.Add(hexCoord.ToString(), instance);
 				}
+				yield return new WaitForSeconds(0.01f);
+				
 			}
 			// StaticBatchingUtility.Combine( this.gameObject );
 		}
-		
 		// Update is called once per frame
 		void Update () {
+			Vector2 mousePosition = Input.mousePosition;
+			var worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 			
+			var hexPosition = HexCoord.AtPosition(worldPosition).ToString();
+			if (World.ContainsKey(hexPosition)){
+				var selectedHexGO = World[hexPosition];
+				selectedHexGO.GetComponent<Hex>().Selected = true;
+			}
 		}
 
 	}
